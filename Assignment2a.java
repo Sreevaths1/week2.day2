220091a3d4948a388bf03d09eaae7b946e10b5c6
Sreevaths1 <95237732+Sreevaths1@users.noreply.github.com>
2021-12-10 22:05:49 +0530

    Add files via upload

---
+++ Assignment2a.java
@@ -0,0 +1,57 @@
+package week2.day2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment2a {
+	
+	public static void main(String[] args)
+	{
+		WebDriverManager.chromedriver().setup();		
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().window().maximize();
+	    String title=driver.getTitle();
+		System.out.println(title);
+		
+		driver.findElement(By.id("username")).sendKeys("DemosalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		
+		String title1=driver.getTitle();
+		System.out.println(title1);
+		
+		driver.findElement(By.linkText("Create Contact")).click();
+		driver.findElement(By.id("firstNameField")).sendKeys("Sreevathsav");
+		driver.findElement(By.id("lastNameField")).sendKeys("Ammayappan");
+		driver.findElement(By.id("createContactForm_firstNameLocal")).sendKeys("Sree");
+		driver.findElement(By.id("createContactForm_lastNameLocal")).sendKeys("vathsav");
+		driver.findElement(By.id("createContactForm_departmentName")).sendKeys("Mechanical");
+		driver.findElement(By.id("createContactForm_description")).sendKeys("random thoughts");
+		driver.findElement(By.id("createContactForm_primaryEmail")).sendKeys("newmail@newmail.com");
+		
+		
+		WebElement elementdropdown3=(driver.findElement(By.id("createContactForm_generalStateProvinceGeoId")));		
+		Select ff =new Select(elementdropdown3);
+		ff.selectByVisibleText("California");	
+		
+		driver.findElement(By.name("submitButton")).click();
+		
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateContactForm_description")).clear();
+		driver.findElement(By.id("updateContactForm_description")).sendKeys("funny thoughts");
+		driver.findElement(By.xpath("//input [@value='Update']")).click();
+		
+		String title2=driver.getTitle();
+		System.out.println(title2);
+		
+		//driver.close();
+		
+	}
+
+}
